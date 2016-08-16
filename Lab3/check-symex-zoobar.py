d4cf03286ac741c348bd0e340ba6394ccb45e6d6
Niklas Särökaari <sarokaari.niklas@gmail.com>
2016-08-16 18:39:55 +0300

    check for zoobar theft working

--- Lab3/check-symex-zoobar.py
+++ Lab3/check-symex-zoobar.py
@@ -85,8 +85,8 @@ def test_stuff():
 
   ## Detect zoobar theft.
   ## When detected, call report_zoobar_theft()
-if balance != balance1 - sum([p.amount for p in tdb.query(zoobar.zoodb.Transfer).all()]):
-   report_zoobar_theft()
+  if balance != balance1 - sum([p.amount for p in tdb.query(zoobar.zoodb.Transfer).all()]):
+     report_zoobar_theft()
 
 fuzzy.concolic_test(test_stuff, maxiter=2000, verbose=1)
 
